113f69584499aa17e34d85ebefdfea0dba969394
Brian Cain <brian.cain@oss.qualcomm.com>
2026-03-26 13:23:05 -0700

    RPython targetnopstandalone: add JIT driver for JIT testing
    Add a simple JitDriver-based loop to targetnopstandalone so it can be
    used with -Ojit to verify JIT compilation on the Hexagon backend.
    Computes sum(1..1100)=605550 as a basic JIT smoke test.

--- rpython/translator/goal/targetnopstandalone.py
+++ rpython/translator/goal/targetnopstandalone.py
@@ -7,13 +7,27 @@ of input types is empty, it is meant to be a list of strings,
 actually implementing argv of the executable.
 """
 
+from rpython.rlib.jit import JitDriver
+
+jitdriver = JitDriver(greens=[], reds=['n', 'total'])
+
 def debug(msg):
     print "debug:", msg
+
+def jitloop(n):
+    total = 0
+    while n > 0:
+        jitdriver.jit_merge_point(n=n, total=total)
+        total = total + n
+        n = n - 1
+    return total
 
 # __________  Entry point  __________
 
 def entry_point(argv):
     debug("hello world")
+    result = jitloop(1100)
+    debug("result = " + str(result))
     return 0
 
 # _____ Define and setup target ___
